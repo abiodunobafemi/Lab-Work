2d3ac21e29616e3738cf5adbd6a9b87cd99b3844
abiodunobafemi <59652204+abiodunobafemi@users.noreply.github.com>
2021-02-20 22:25:12 -0500

    Create Lab1-2 #2.java

---
+++ Lab1-2 #2.java
@@ -0,0 +1,17 @@
+// Abiodun Obafemi
+
+public class Arithmetic {
+
+		public static void main(String[] args) {
+			
+			System.out.println(9 + 5);
+			System.out.println(9 - 5);
+			System.out.println(3 * 7);
+			System.out.println(7 / 2);
+			System.out.println(7 / 2.0);
+			System.out.println(10 / 2);
+			System.out.println(7 % 2);
+			System.out.println (16 % 4);
+		
+	}
+}
