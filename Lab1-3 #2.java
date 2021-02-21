d1bfcf57ab4a8ac9af92c6560eff75f3437061d6
abiodunobafemi <59652204+abiodunobafemi@users.noreply.github.com>
2021-02-20 22:31:31 -0500

    Create Lab1-3 #2.java

---
+++ Lab1-3 #2.java
@@ -0,0 +1,19 @@
+// Abiodun Obafemi
+
+public class Table {
+
+	public static void main(String[] args) {
+		
+		// Display the following table
+		// a  a^2 a^3
+		// 1   1   1
+		// 2   4   8
+		// 3   9   27
+		
+		System.out.println("a   a^2   a^3");
+		System.out.println("1   1     1");
+		System.out.println("2   4     8");
+		System.out.println("3   9     27");
+	}
+
+}
