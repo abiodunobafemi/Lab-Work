2dd59b129214835ca734eed13d5822c846baed59
abiodunobafemi <59652204+abiodunobafemi@users.noreply.github.com>
2021-02-20 22:28:06 -0500

    Create Lab1-3.java

---
+++ Lab1-3.java
@@ -0,0 +1,12 @@
+// Abiodun Obafemi
+
+public class Difference {
+	
+	public static void main(String[] args) {
+		
+		// What is the difference?
+		System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
+		System.out.print((10.5 + 2 * 3) / (45 - 3.5));
+		
+		}
+}
