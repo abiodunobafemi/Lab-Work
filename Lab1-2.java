ec8c65a6529304b63a5f4da0e06ed253f9e68adb
abiodunobafemi <59652204+abiodunobafemi@users.noreply.github.com>
2021-02-20 22:25:52 -0500

    Update Lab1-2.java

--- Lab1-2.java
+++ Lab1-2.java
@@ -1,16 +1,15 @@
 // Abiodun Obafemi
 
-public class Lab2 {
-
-	public class TestPrintPrintln {
-		public static void main(String[] args) {
-		System.out.println("Printing with print");
-		System.out.print("Hello");
-		System.out.print("World");
-		System.out.println("Printing with println");
-		System.out.println("Hello");
-		System.out.println("World");
+public class TestPrintPrintln {
+	public static void main(String[] args) {
 		
-		}
+	System.out.println("Printing with print");
+	System.out.print("Hello");
+	System.out.print("World");
+	System.out.println("Printing with println");
+	System.out.println("Hello");
+	System.out.println("World");
+		
 	}
 }
+
