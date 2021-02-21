a59ddd585e481570423df288473cbc22fe01b1c8
abiodunobafemi <59652204+abiodunobafemi@users.noreply.github.com>
2021-02-20 22:33:20 -0500

    Create Lab1-3 #3.java

---
+++ Lab1-3 #3.java
@@ -0,0 +1,20 @@
+// Abiodun Obafemi
+
+public class MoreArithmetic {
+
+	public static void main(String[] args) {
+		
+		// Display the following console
+		// pi is 3.141592653589793
+		// pi * 2 is 6.283185307179586
+		// pi^2 is 9.869604401089358
+		
+		System.out.print("pi is ");
+		System.out.println(Math.PI);
+		System.out.print("pi * 2 is ");
+		System.out.println(Math.PI * 2);
+		System.out.print("pi^2 is ");
+		System.out.println(Math.pow(Math.PI, 2));
+	}
+
+}
