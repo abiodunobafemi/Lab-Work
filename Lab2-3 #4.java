af9498f17f690211d39a44db7f4fddb6082cc568
abiodunobafemi <59652204+abiodunobafemi@users.noreply.github.com>
2021-02-28 18:02:24 -0500

    Update Lab2-3 #4.java

--- Lab2-3 #4.java
+++ Lab2-3 #4.java
@@ -1,4 +1,13 @@
 // Abiodun Obafemi
+// Write a class called MyCalculator2 which does the following:
+// Look at the Sample Run below first which will help you understand better.
+// Declare 4 variables as double type: inputa, inputb, inputc, total
+// Declare 1 variable as int type: intPart
+// Create a Scanner.
+// Asks the user to enter input a, b, c. (note which line the user inputs)
+// Calculate the result and store in the variable total.
+// Use casting to show only the integer part of the result.
+// Show the output.
 
 import java.util.*;
 public class MyCalculator2 {
