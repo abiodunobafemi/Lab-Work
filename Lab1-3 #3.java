fe952b0c91681fad22ab2c8fe7e180a7c8afee99
abiodunobafemi <59652204+abiodunobafemi@users.noreply.github.com>
2021-02-28 17:58:59 -0500

    Update Lab1-3 #3.java

--- Lab1-3 #3.java
+++ Lab1-3 #3.java
@@ -1,14 +1,14 @@
 // Abiodun Obafemi
+// Write a Java program called MoreArithmetic using java.lang.Math classes that displays the following on the console.
+// pi is 3.141592653589793
+// pi * 2 is 6.283185307179586
+// pi^2 is 9.869604401089358
 
 public class MoreArithmetic {
 
 	public static void main(String[] args) {
 		
-		// Display the following console
-		// pi is 3.141592653589793
-		// pi * 2 is 6.283185307179586
-		// pi^2 is 9.869604401089358
-		
+		// Displays the statements
 		System.out.print("pi is ");
 		System.out.println(Math.PI);
 		System.out.print("pi * 2 is ");
